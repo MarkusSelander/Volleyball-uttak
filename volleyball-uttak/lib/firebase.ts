08307356f4c9774d93ed29a803468fd0d5fd16a9
Markus Selander <markus_selander@outlook.com>
2025-08-11 17:54:40 +0200

    Fikset depolyment

--- volleyball-uttak/lib/firebase.ts
+++ volleyball-uttak/lib/firebase.ts
@@ -1,23 +1,29 @@
-import { initializeApp, getApps, type FirebaseApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, type Auth } from 'firebase/auth';
-import { getFirestore, type Firestore } from 'firebase/firestore';
+import { getApps, initializeApp, type FirebaseApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+  type Auth,
+} from "firebase/auth";
+import { getFirestore, type Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
-  apiKey: 'AIzaSyDjr446XPjcDNoslcMAl6swH81A0SDNoOo',
-  authDomain: 'volleyball-d2c.firebaseapp.com',
-  projectId: 'volleyball-d2c',
-  storageBucket: 'volleyball-d2c.firebasestorage.app',
-  messagingSenderId: '880881760876',
-  appId: '1:880881760876:web:e00922adf7eb5347b63649',
-  measurementId: 'G-FPS2B2X91T',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 let app: FirebaseApp | undefined;
 let auth: Auth;
 let db: Firestore;
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
   auth = getAuth(app);
   db = getFirestore(app);
@@ -25,4 +31,4 @@ if (typeof window !== 'undefined') {
 
 export { auth, db };
 export const googleProvider = new GoogleAuthProvider();
-export { signInWithPopup, signOut, onAuthStateChanged };
+export { onAuthStateChanged, signInWithPopup, signOut };
